749fa347dac0fff21a6fee513d5b4ee7693d5f4e
Mohab Gabber <mohabgabber1@protonmail.com>
2022-01-06 01:10:03 +0200

    added special chars

--- script.py
+++ script.py
@@ -7,7 +7,7 @@ under certain conditions.
 	\n''')
 def remove(passwords):
     return passwords.replace(" ", "")
-alphanumeric = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', ' ']
+alphanumeric = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', ' ', '?', '!', '#', '@', '%', '$', '^', '&', '*', '(', ')', '[', ']', '{', '}', '+', '=', '-', '_', '~', '\\', '/', "'", '"', ';', ':', '<', '>', '.', ',']
 options = str(input("Do You Want To Encrypt or Decrypt? (E/D): ").lower())
 message = str(input("Please Enter Your Message: ").lower())
 password = str(input("Enter Your Password: ").lower())
