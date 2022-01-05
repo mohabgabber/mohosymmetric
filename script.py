fcf38849e7574be074ffd4af8b22d714f9848b39
Mohab Gabber <mohabgabber1@protonmail.com>
2022-01-05 23:24:04 +0200

    added some fixes

--- script.py
+++ script.py
@@ -1,30 +1,31 @@
 print("Hi, The Purpose Of This Software Is To Do A Basic Encoding Algorith, Made By Mohab Gabber\n ")
-options = str(input("Do You Want To Encrypt or Decrypt? (E/D): ").lower())
-message = str(input("Please Enter Your Message: ").lower())
-password = str(input("Enter Your Password: ").lower())
 def remove(passwords):
     return passwords.replace(" ", "")
 alphanumeric = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+options = str(input("Do You Want To Encrypt or Decrypt? (E/D): ").lower())
+message = str(input("Please Enter Your Message: ").lower())
+password = str(input("Enter Your Password: ").lower())
+keychars = []
+positions = []
+finalpositions = []
+for i in remove(password):
+	keychars.append(i)
+for s in keychars:
+	positions.append(alphanumeric.index(s))
+for e in positions:
+	item = e + 3
+	finalpositions.append(item)
 if options == 'e':
-	positions = []
-	actualpositions = []
-	finalpositions = []
-	for i in remove(password):
-		positions.append(i)
-	for i in positions:
-		actualpositions.append(alphanumeric.index(i))
-	for i in actualpositions:
-		item = i + 3
-		finalpositions.append(item)
 	for i in message:
 		encrypted = ''
 		if i == ' ':
-			encrypted += '0'
+			encrypted += ' '
 		else:
+			count = 0
 			for j in finalpositions:
 				indexofitem = alphanumeric.index(i)
 				fixedindex = indexofitem + j 
-				encrypted += alphanumeric[fixedindex]
+				encrypted += alphanumeric[fixedindex % 36]
 	print(encrypted)
 #elif options == 'd':
 else:
