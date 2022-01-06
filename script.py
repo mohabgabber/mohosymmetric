0da0ff1a12002eceb6543a651cf68ff4513ae165
Mohab Gabber <mohabgabber1@protonmail.com>
2022-01-07 01:50:08 +0200

    added vigenere option

--- script.py
+++ script.py
@@ -23,37 +23,41 @@ This program comes with ABSOLUTELY NO WARRANTY.
 This is free software, and you are welcome to redistribute it
 under certain conditions.
 	\n''')
-alphanumeric = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', ' ', '?', '!', '#', '@', '%', '$', '^', '&', '*', '(', ')', '[', ']', '{', '}', '+', '=', '-', '_', '~', '\\', '/', "'", '"', ';', ':', '<', '>', '.', ',']
-options = input("Do You Want To Encrypt or Decrypt? (E/D): ").lower()
-message = input("Please Enter Your Message: ")
-plaintext_password = input("Enter Your Password: ")
-hashed = str(hashlib.sha256(plaintext_password.encode('utf-8')).hexdigest())
-shifts = int(input("How Many Shifts Do You Want: "))
-keychars = []
-positions = []
-finalpositions = []
-key = 0
-for i in hashed:
-	keychars.append(i)
-for s in keychars:
-	positions.append(alphanumeric.index(s))
-for e in positions:
-	item = e + shifts
-	finalpositions.append(item)
-for p in finalpositions:
-	key += p
-if options == 'e':
-	encrypted = ''
-	for i in message:
-		indexed = (alphanumeric.index(i) + key + 2) % len(alphanumeric)
-		encrypted += alphanumeric[indexed]
-	print(encrypted)
-elif options == 'd':
-	decrypted = ''
-	for i in message:
-		indexed = (alphanumeric.index(i) - key - 2) % len(alphanumeric)
-		decrypted += alphanumeric[indexed]
-	print(decrypted)
-else:
-	print("That's A Wrong Choice")
+option1 = input("Do You Want To Use Caesar Or Vigenere Cipher? (C/V): ").lower()
+if option1 == 'c':
+	alphanumeric = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', ' ', '?', '!', '#', '@', '%', '$', '^', '&', '*', '(', ')', '[', ']', '{', '}', '+', '=', '-', '_', '~', '\\', '/', "'", '"', ';', ':', '<', '>', '.', ',']
+	option_caesar2 = input("Do You Want To Encrypt or Decrypt? (E/D): ").lower()
+	message = input("Please Enter Your Message: ")
+	plaintext_password = input("Enter Your Password: ")
+	hashed = str(hashlib.sha256(plaintext_password.encode('utf-8')).hexdigest())
+	shifts = int(input("How Many Shifts Do You Want: "))
+	keychars = []
+	positions = []
+	finalpositions = []
+	key = 0
+	for i in hashed:
+		keychars.append(i)
+	for s in keychars:
+		positions.append(alphanumeric.index(s))
+	for e in positions:
+		item = e + shifts
+		finalpositions.append(item)
+	for p in finalpositions:
+		key += p
+	if option_caesar2 == 'e':
+		encrypted = ''
+		for i in message:
+			indexed = (alphanumeric.index(i) + key + 2) % len(alphanumeric)
+			encrypted += alphanumeric[indexed]
+		print(encrypted)
+	elif option_caesar2 == 'd':
+		decrypted = ''
+		for i in message:
+			indexed = (alphanumeric.index(i) - key - 2) % len(alphanumeric)
+			decrypted += alphanumeric[indexed]
+		print(decrypted)
+	else:
+		print("That's A Wrong Choice")
+elif option1 == 'v':
+	pass
 	
